6874cd9782c42e4c2f8611bdf1c03f3344d3236d
Julien Sautereau <julien.sautereau@etu.univ-lehavre.fr>
2018-01-24 18:33:12 +0100

    Initial Commit

--- js/maintenance.js
+++ js/maintenance.js
@@ -7,12 +7,38 @@
  * Function Heure Courante
  */
 
-function heureCourante {
+function heureCourante() {
 
   var date = new Date();
-  var jour = "Il est : " + date.getDay();
+  var jour = "Il est : " + date.getTime();
 
-  document.getElementsById('timeNow').innerHTML = jour;
+  var timeNow = document.getElementById('timeNow');
+
+  var str = "";
+
+    var currentTime = new Date()
+    var hours = currentTime.getHours()
+    var minutes = currentTime.getMinutes()
+    var seconds = currentTime.getSeconds()
+
+    if (minutes < 10) {
+        minutes = "0" + minutes
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds
+    }
+    str += hours + ":" + minutes + ":" + seconds + " ";
+
+    timeNow.innerHTML = str;
 }
+
+function afficheHeure() {
+
+  setInterval(heureCourante, 1000);
+
+}
+
+afficheHeure();
+
 
 heureCourante();
